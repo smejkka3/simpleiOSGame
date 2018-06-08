ec00318a56b0d1ff399ad43adc70b2a34668082b
Karel Smejkal <karel.smejkal@campus.tu-berlin.de>
2018-06-08 17:54:36 +0200

    animation logic

--- myGame/ShooterScene.swift
+++ myGame/ShooterScene.swift
@@ -10,5 +10,22 @@ import UIKit
 import SpriteKit
 
 class ShooterScene: SKScene {
+    
+    var score = 0
+    var enemyCount = 10
+    var shooterAnimation = [SKTexture]()
 
+    override func didMove(to view: SKView) {
+        self.initShooterScene()
+        
+    }
+    
+    func initShooterScene(){
+        let shooterAtlas = SKTextureAtlas(named: "Shooter")
+        
+        for index in 1...shooterAtlas.textureNames.count{
+            let imgName = String(format: "Shoot%01d", index)
+            shooterAnimation += [shooterAtlas.textureNamed(imgName)]
+        }
+    }
 }
